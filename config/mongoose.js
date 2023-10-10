3eeabceabc8d4a3612edcd339d3a04c043a3d947
Rohit Saini <rs0118032@gmail.com>
2023-10-10 23:16:02 +0530

    main data based linked and controller code updates to return a list(array) of defaulters

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://127.0.0.1/pollingAPI`);
+mongoose.connect(`mongodb+srv://assignment:edviron@cluster0.ebxruu8.mongodb.net`);
 
 const db= mongoose.connection;
 
 db.on('erro',console.error.bind(console,"Error in connection to database"));
 
 db.once('open', function(){
-    console.log('Database Connected');
+    console.log('Database Connected Successfully !!');
 });
 
 module.exports = db;
--- controllers/defaultersController.js
+++ controllers/defaultersController.js
@@ -1,23 +1,38 @@
 const mongoose= require("mongoose");
 const db = require('../config/mongoose');
 
-const Que = mongoose.model('questions',new mongoose.Schema({}, { strict: false }), 'questions');
-
+// const Que = mongoose.model('questions',new mongoose.Schema({}, { strict: false }), 'questions');
+const Students = mongoose.model('students',new mongoose.Schema({}, { strict: false }), 'students');
+const Invoices = mongoose.model('invoices',new mongoose.Schema({}, { strict: false }), 'invoices');
 
 
 module.exports.findDefaultees = async function(req,res){
     
     try {
-        const que_id = '64d0c27f5cfee53b7ea895d8';
-        
-        const getOne= await Que.findById(que_id);
-        
-        if(getOne){
-            res.send(getOne);
+        const defaultees= await Students.aggregate([
+            {
+              $lookup: {
+                from: "invoices",
+                localField: "_id",
+                foreignField: "student",
+                as: "invoices"
+              }
+            },
+            {
+              $unwind: "$invoices"
+            },
+            {
+              $match: {
+                "invoices.status": "unpaid"
+              }
+            }
+        ]);
+        if(defaultees){
+            res.send(defaultees);
         }
 
     } catch (error) {
-        res.send(`Database error: can't the given student: ${error}`)
+        res.send(`Database error: can't data: ${error}`)
         // console.log("Database error: can't the given student \n ",error);
     }
 
